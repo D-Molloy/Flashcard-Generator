1e87a64e541e3b070a11a5165fdefb88410bc440
Denis Molloy <denis.molloy@gmail.com>
2017-09-11 14:06:07 -0400

    ClozeCard.js and main.js updated

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,7 +1,24 @@
+var jsonfile = require('jsonfile');
+jsonfile.spaces = 2;
+var file = 'data.json'
+var cardArray = [];
+
 var ClozeCard = function(text, cloze){
     this.text = text;
     this.cloze = cloze;
     this.partial = text.replace(cloze, '...');
+}
+
+ClozeCard.prototype.createCloze = function () {
+    
+    var obj = {
+        frontOfCard: this.partial,
+        backOfCard: this.cloze
+    };
+
+    cardArray.push(obj);
+    // , {flag: 'a'}
+    jsonfile.writeFileSync(file, cardArray);
 }
 
 module.exports = ClozeCard;
--- main.js
+++ main.js
@@ -17,8 +17,8 @@ function appStart(){
     ]).then(function(inquirerResponse){
         if (inquirerResponse.mainOptions == "Study existing cards"){
             fs.readFile(file, "utf8", function(err, obj) {
-                // var oneCard = obj[0].frontOfCard;
-                console.log(obj);
+                var oneCard = Object.keys(obj)[0].frontOfCard;
+                console.log(oneCard);
               });
         } else if (inquirerResponse.mainOptions == "Create a new basic flashcard") {
             function basicPrompt(){
@@ -33,35 +33,80 @@ function appStart(){
                         message: "Please enter the answer to the question you just inputted:",
                         name: "cardBack"
                     }
-                ]).then(function(inquirerResponse){
-                    if (inquirerResponse.cardFront != "" && inquirerResponse.cardBack != "" ) {
-                        var newCard = new BasicCard(inquirerResponse.cardFront, inquirerResponse.cardBack);
-                        newCard.createBasic();
-                        console.log("Basic Card Created!");
-                        inquirer.prompt([
-                            {
-                                type: "list",
-                                message: "Would you like to create another basic flashcard?",
-                                choices: ["Yes", "No"],
-                                name: "basicRestart"
-                            }
-                        ]).then(function(inquirerResponse){
-                            if (inquirerResponse.basicRestart == "Yes") {
-                                basicPrompt();
-                            } else if (inquirerResponse.basicRestart == "No"){
-                                appStart();
-                            }
-                        })
+                ]).then(function(inquirerResponse){               
+                    if (inquirerResponse.cardFront != "" && inquirerResponse.cardBack != "" ) {                               
+                            var newBasic = new BasicCard(inquirerResponse.cardFront, inquirerResponse.cardBack);
+                            newBasic.createBasic();
+                            console.log("Basic Flashcard Created!");
+                            inquirer.prompt([
+                                {
+                                    type: "list",
+                                    message: "Would you like to create another basic flashcard?",
+                                    choices: ["Yes", "No"],
+                                    name: "basicRestart"
+                                }
+                            ]).then(function(inquirerResponse){
+                                if (inquirerResponse.basicRestart == "Yes") {
+                                    basicPrompt();
+                                } else if (inquirerResponse.basicRestart == "No"){
+                                    appStart();
+                                }
+                            })
                     } else {
                         console.log("Please input an answer for both sides of the flash card.");
                         basicPrompt();
                     }
-    
                 })
             }
             basicPrompt();
         } else if (inquirerResponse.mainOptions == "Create a new cloze flashcard") {
-            console.log("creating a new cloze flashcard")
+            function clozePrompt(){
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "****Cloze Flashcard Generator****\nPlease enter the full text of the statment you want to study:",
+                        name: "cardFront"
+                    },
+                    {
+                        type: "input",
+                        message: "Please enter cloze keyword(s):",
+                        name: "cardBack"
+                    }
+                ]).then(function(inquirerResponse){
+
+                    var checkString = inquirerResponse.cardFront;
+
+                    if (inquirerResponse.cardFront != "" && inquirerResponse.cardBack != "" ) {
+                        if (checkString.includes(inquirerResponse.cardBack)){
+                            var newCloze = new ClozeCard(inquirerResponse.cardFront, inquirerResponse.cardBack);
+                            newCloze.createCloze();
+                            console.log("Cloze Flashcard Created!");
+                            inquirer.prompt([
+                                {
+                                    type: "list",
+                                    message: "Would you like to create another cloze flashcard?",
+                                    choices: ["Yes", "No"],
+                                    name: "clozeRestart"
+                                }
+                            ]).then(function(inquirerResponse){
+                                if (inquirerResponse.clozeRestart == "Yes") {
+                                    clozePrompt();
+                                } else if (inquirerResponse.clozeRestart == "No"){
+                                    appStart();
+                                }
+                            })
+                        } else {
+                            console.log("Cloze keyword not found in text!  Please input the cloze keyword as it appears in the full text you submit.")
+                            clozePrompt();
+                        }
+                    } else {
+                        console.log("Please input an answer for both sides of the flash card.");
+                        clozePrompt();
+                    }
+    
+                })
+            }
+            clozePrompt();
         }
     })
 };
